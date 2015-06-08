3108d11c637f856df370f508e33f1b552ae8b45d
Dan Klos <dsklos@gmail.com>
2015-06-08 13:25:01 -0400

    Still trying to access compose window contents

--- js/compose.js
+++ js/compose.js
@@ -1,5 +1,30 @@
-if ($(".aoI")) {
-  console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText.length);
-}
+$(".z0").ready(function() {
 
-// Need to require JQuery
+  debugger;
+
+  $(".Ap div:nth-child(2) div:nth-child(1)").ready(function() {
+    debugger;
+    console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText.length);
+  })
+
+  $(".z0 div:nth-child(1)").on("click", function() {
+
+    console.log("MADE IT HERE")
+
+  })
+})
+
+
+// var insertListener = function(event){
+//   debugger;
+//   if (event.animationName == "nodeInserted") {
+//     debugger;
+//     // This is the debug for knowing our listener worked!
+//     // event.target is the new node!
+//     console.warn("Another node has been inserted! ", event, event.target);
+//   }
+// };
+
+// document.addEventListener("webkitAnimationStart", insertListener, false);
+
+
