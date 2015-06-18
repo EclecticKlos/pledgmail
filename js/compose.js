c742aef1a1d34789ee296a15bac415424596241b
Dan Klos <dsklos@gmail.com>
2015-06-18 16:53:47 -0700

    Detects keyup in the compose box

--- js/compose.js
+++ js/compose.js
@@ -1,57 +1,21 @@
-
-// $(".Am").ready(function() {
-
-  // $(".Ap div:nth-child(2) div:nth-child(1)").ready(function() {
-  //   console.log("hereeeeeeeee")
-  //   console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText)
-  //   console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText.length);
-  // })
-
-  // $(".z0 div:nth-child(1)").on("click", function() {
-
-  //   console.log("MADE IT HERE")
-
-  // })
-  // console.log($(".Am").innerText)
-  // var charPresent = $(".Am").innerText;
-  // console.log("^This")
-  // while (charPresent == undefined){
-  //   console.log("Yes I am undefined")
-  //   setInterval(function() {
-  //     charPresent = $(".Am").innerText
-  //     console.log(charPresent)
-  //   }, 1000 )
-  // }
-
-
-// })
-
-
-
-
-
-
-
 var insertListener = function(event){
 
   if (event.animationName == "nodeInserted") {
-    // debugger;
-    // This is the debug for knowing our listener worked!
-    // event.target is the new node!
-    console.warn("Another node has been inserted! ", event, event.target);
+    // Thanks to David Walsh (http://davidwalsh.name/detect-node-insertion) for this animation trick
 
-    var charPresent = undefined;
-
-    var checkCharPresent = window.setInterval(function() {
-      charPresent = $(".Am").text();
-      console.log(charPresent + " I am before condition")
-      if (charPresent != undefined) {
-        console.log(charPresent + " I am inside condition")
-        console.log(charPresent.length)
-        window.clearInterval(checkCharPresent);
-        return charPresent;
+    var checkForCharactersInCompose = window.setInterval(function() {
+      charactersInCompose = $(".Am").text();
+      if (charactersInCompose != undefined || charactersInCompose != "") {
+        console.log(charactersInCompose.length);
+        window.clearInterval(checkForCharactersInCompose);
+        return charactersInCompose;
       }
-    }, 1000 )
+    }, 1000);
+
+    $('.Am').keyup(function(event) {
+      console.log("Keyup detected")
+    })
+
   }
 };
 
