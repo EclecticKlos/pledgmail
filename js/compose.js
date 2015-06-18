a81e1bb6305a0892df9dc884fadc208a9ee63dcb
Dan Klos <dsklos@gmail.com>
2015-06-18 13:25:52 -0700

    Finally returns existing text. Yeeeeehaw

--- js/compose.js
+++ js/compose.js
@@ -1,21 +1,33 @@
 
-// $(".z0").ready(function() {
+// $(".Am").ready(function() {
 
-//   $(".Ap div:nth-child(2) div:nth-child(1)").ready(function() {
-//     console.log("hereeeeeeeee")
-//     console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText)
-//     console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText.length);
-//   })
+  // $(".Ap div:nth-child(2) div:nth-child(1)").ready(function() {
+  //   console.log("hereeeeeeeee")
+  //   console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText)
+  //   console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText.length);
+  // })
 
-//   $(".z0 div:nth-child(1)").on("click", function() {
+  // $(".z0 div:nth-child(1)").on("click", function() {
 
-//     console.log("MADE IT HERE")
+  //   console.log("MADE IT HERE")
 
-//   })
-
+  // })
+  // console.log($(".Am").innerText)
+  // var charPresent = $(".Am").innerText;
+  // console.log("^This")
+  // while (charPresent == undefined){
+  //   console.log("Yes I am undefined")
+  //   setInterval(function() {
+  //     charPresent = $(".Am").innerText
+  //     console.log(charPresent)
+  //   }, 1000 )
+  // }
 
 
 // })
+
+
+
 
 
 
@@ -27,6 +39,19 @@ var insertListener = function(event){
     // This is the debug for knowing our listener worked!
     // event.target is the new node!
     console.warn("Another node has been inserted! ", event, event.target);
+
+    var charPresent = undefined;
+
+    var checkCharPresent = window.setInterval(function() {
+      charPresent = $(".Am").text();
+      console.log(charPresent + " I am before condition")
+      if (charPresent != undefined) {
+        console.log(charPresent + " I am inside condition")
+        console.log(charPresent.length)
+        window.clearInterval(checkCharPresent);
+        return charPresent;
+      }
+    }, 1000 )
   }
 };
 
