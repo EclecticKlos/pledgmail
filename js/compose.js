2c59d6bb7d361f42b9cb7b1e072bf851f81cda07
Dan Klos <dsklos@gmail.com>
2015-06-17 22:13:15 -0700

    animation hack appears to work, needs further testing

--- js/compose.js
+++ js/compose.js
@@ -1,30 +1,33 @@
-$(".z0").ready(function() {
 
-  debugger;
+// $(".z0").ready(function() {
 
-  $(".Ap div:nth-child(2) div:nth-child(1)").ready(function() {
-    debugger;
-    console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText.length);
-  })
+//   $(".Ap div:nth-child(2) div:nth-child(1)").ready(function() {
+//     console.log("hereeeeeeeee")
+//     console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText)
+//     console.log($(".Ap div:nth-child(2) div:nth-child(1)").innerText.length);
+//   })
 
-  $(".z0 div:nth-child(1)").on("click", function() {
+//   $(".z0 div:nth-child(1)").on("click", function() {
 
-    console.log("MADE IT HERE")
+//     console.log("MADE IT HERE")
 
-  })
-})
-
-
-// var insertListener = function(event){
-//   debugger;
-//   if (event.animationName == "nodeInserted") {
-//     debugger;
-//     // This is the debug for knowing our listener worked!
-//     // event.target is the new node!
-//     console.warn("Another node has been inserted! ", event, event.target);
-//   }
-// };
-
-// document.addEventListener("webkitAnimationStart", insertListener, false);
+//   })
 
 
+
+// })
+
+
+
+
+var insertListener = function(event){
+
+  if (event.animationName == "nodeInserted") {
+    // debugger;
+    // This is the debug for knowing our listener worked!
+    // event.target is the new node!
+    console.warn("Another node has been inserted! ", event, event.target);
+  }
+};
+
+document.addEventListener("webkitAnimationStart", insertListener, false);
