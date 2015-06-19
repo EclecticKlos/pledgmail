98d34ee0031a0c6f5c406f51e4823716542a1e24
Dan Klos <dsklos@gmail.com>
2015-06-19 11:38:52 -0700

    add count to compose window, however it adds each keystroke as a new element

--- js/compose.js
+++ js/compose.js
@@ -1,27 +1,85 @@
 var insertListener = function(event){
 
   if (event.animationName == "nodeInserted") {
-    // Thanks to David Walsh (http://davidwalsh.name/detect-node-insertion) for this animation trick
+    // Thanks to David Walsh (http://davidwalsh.name/detect-node-insertion) for this animation listener trick
+
+    var displayCharCount = function(charCount) {
+      var span = document.createElement('span');
+      span.textContent = charCount;
+      span.setAttribute('class', 'count');
+      $("tr.n1tfz td:nth-child(5) div")[0].appendChild(span)
+    }
+
+    var keyupListener = function() {
+      $('.Am').keyup(function(event) {
+        console.log("Keyup detected")
+        charactersInCompose = $(".Am").text();
+        console.log(charactersInCompose.length);
+        var charCount = charactersInCompose.length;
+        displayCharCount(charCount);
+      })
+    }
 
     var charactersInCompose = $(".Am").text();
+    var updatedCharactersInCompose = undefined;
     var checkForCharactersInCompose = window.setInterval(function() {
-      if (charactersInCompose != undefined || charactersInCompose != "") {
+      if (charactersInCompose != undefined || charactersInCompose != '') {
         console.log("Inside the interval");
+        console.log(charactersInCompose);
         console.log(charactersInCompose.length);
         window.clearInterval(checkForCharactersInCompose);
-        return charactersInCompose;
+      keyupListener();
       }
+
     }, 1000);
 
-    var updatedCharactersInCompose = charactersInCompose;
-
-    $('.Am').keyup(function(event) {
-      console.log("Keyup detected")
-      updatedCharactersInCompose = $(".Am").text();
-      console.log(updatedCharactersInCompose.length);
-    })
 
   }
 };
 
 document.addEventListener("webkitAnimationStart", insertListener, false);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var insertListener = function(event){
+
+//   if (event.animationName == "nodeInserted") {
+//     // Thanks to David Walsh (http://davidwalsh.name/detect-node-insertion) for this animation listener trick
+
+//     var charactersInCompose = $(".Am").text();
+//     var updatedCharactersInCompose = undefined;
+//     var checkForCharactersInCompose = window.setInterval(function() {
+//       if (charactersInCompose != undefined || charactersInCompose != "") {
+//         console.log("Inside the interval");
+//         console.log(charactersInCompose.length);
+//         window.clearInterval(checkForCharactersInCompose);
+//         // return charactersInCompose;
+//       }
+//     }, 1000);
+
+//     console.log(charactersInCompose + " this test");
+//     var updatedCharactersInCompose = charactersInCompose;
+
+//     $('.Am').keyup(function(event) {
+//       console.log("Keyup detected")
+//       updatedCharactersInCompose = $(".Am").text();
+//       console.log(updatedCharactersInCompose.length);
+//     })
+
+//   }
+// };
+
+// document.addEventListener("webkitAnimationStart", insertListener, false);
