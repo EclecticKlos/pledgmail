d9d747eecd49250c8d05b138afaea67e91b6a683
Dan Klos <dsklos@gmail.com>
2015-06-18 18:29:53 -0700

    Updates the character count from keyup

--- js/compose.js
+++ js/compose.js
@@ -3,17 +3,22 @@ var insertListener = function(event){
   if (event.animationName == "nodeInserted") {
     // Thanks to David Walsh (http://davidwalsh.name/detect-node-insertion) for this animation trick
 
+    var charactersInCompose = $(".Am").text();
     var checkForCharactersInCompose = window.setInterval(function() {
-      charactersInCompose = $(".Am").text();
       if (charactersInCompose != undefined || charactersInCompose != "") {
+        console.log("Inside the interval");
         console.log(charactersInCompose.length);
         window.clearInterval(checkForCharactersInCompose);
         return charactersInCompose;
       }
     }, 1000);
 
+    var updatedCharactersInCompose = charactersInCompose;
+
     $('.Am').keyup(function(event) {
       console.log("Keyup detected")
+      updatedCharactersInCompose = $(".Am").text();
+      console.log(updatedCharactersInCompose.length);
     })
 
   }
