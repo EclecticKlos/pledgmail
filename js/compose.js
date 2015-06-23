0f90b5da8866cd3dc66128c05646e4309b082dba
Dan Klos <dsklos@gmail.com>
2015-06-23 16:21:55 -0700

    Notification only displays once per overage, does not dispaly again until characters are below max

--- js/compose.js
+++ js/compose.js
@@ -25,10 +25,15 @@ var insertListener = function(event){
     var updateCharCounter = function(charCount) {
       // debugger;
       $("span.count")[0].innerText = (charCount + charCountCharLimitDivider + charLimit);
-      if (charCount > charLimit) {
-        console.log("It's been exceeded!");
+      debugger;
+      if (charCount > charLimit && notificationAlreadyDisplayedOnce === false) {
         charLimitExceededNotification();
+        notificationAlreadyDisplayedOnce = true;
       }
+      else if (charCount > charLimit && notificationAlreadyDisplayedOnce === true) {
+        notificationAlreadyDisplayedOnce = true;
+      }
+      else { notificationAlreadyDisplayedOnce = false; }
     }
 
     var findSignatureLength = function(){
@@ -79,6 +84,7 @@ var insertListener = function(event){
       })
     }
 
+    var notificationAlreadyDisplayedOnce = "";
     var signatureHyphenLength = 0;
     var signatureHyphens = false;
     var charLimit = 640
@@ -105,99 +111,5 @@ var insertListener = function(event){
 };
 
 document.addEventListener("webkitAnimationStart", insertListener, false);
-
-
-
-
-
-
-
-
-// $(".Ap div:nth-child(2) div:nth-child(1)")[0].hasChildNodes()
-// = true
-// THIS IS ALWAYS TRUE
-
-// $(".Ap div:nth-child(2) div:nth-child(1)")[0].children[0].innerText
-// Yields sig
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var insertListener = function(event){
-
-//   if (event.animationName == "nodeInserted") {
-//     // Thanks to David Walsh (http://davidwalsh.name/detect-node-insertion) for this animation listener trick
-
-//     var makeCharCounterSpan = function() {
-//       // span.textContent = charCount;
-//       span.setAttribute('class', 'count');
-//       span.setAttribute('style', 'color: red');
-//       span.style.fontSize = "77%";
-//       $("tr.n1tfz td:nth-child(5) div")[0].appendChild(span)
-//     }
-
-//     var updateCharCounter = function(charCount) {
-//       $("tr.n1tfz td:nth-child(5) div span:nth-child(3)")[0].innerText = (charCount + "" + charLimit);
-//     }
-
-//     var keyupListener = function() {
-//       $('.Am').keyup(function(event) {
-//         console.log("Keyup detected")
-//         charactersInCompose = $(".Am").text();
-//         debugger;
-//         console.log($(".gmail_signature") + " << sig")
-//         if ($(".gmail_signature").text().length !== 0) {
-//           signature = ($(".gmail_signature").text().length + signatureExtraChars);
-//         } else if ($(".Ap div:nth-child(2) div div:nth-child(4)").text().length !== 0) {
-//           signature = ($(".Ap div:nth-child(2) div div:nth-child(4)").text().length + signatureExtraChars);
-//         } else {
-//           signature = 0;
-//         }
-
-//         charCount = (charactersInCompose.length - signature);
-//         console.log(charactersInCompose);
-//         updateCharCounter(charCount);
-//       })
-//     }
-
-//     var charLimit = "/640"
-//     var signature = 0;
-//     var signatureExtraChars = 4;
-//     var span = document.createElement('span');
-//     var charactersInCompose = $(".Am").text();
-//     var charCount = undefined;
-//     var checkForCharactersInCompose = window.setInterval(function() {
-//       if (charactersInCompose != undefined || charactersInCompose != '') {
-//         console.log("Inside the interval");
-//         console.log(charactersInCompose);
-//         console.log(charactersInCompose.length);
-//         window.clearInterval(checkForCharactersInCompose);
-//       makeCharCounterSpan();
-//       keyupListener();
-//       }
-
-//     }, 1000);
-
-
-//   }
-// };
-
-// document.addEventListener("webkitAnimationStart", insertListener, false);
-
-
-
 
 
