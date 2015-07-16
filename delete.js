d2a11830c630883950caaaed77e5e0388a5a0478
Dan Klos <dsklos@gmail.com>
2015-07-16 11:28:20 -0700

    Labels applied to emails that are too long, however labels are not applied to concise emails, the charCount for threads reads the entire thread rather than the latest email, all threads and messages take into account the signature, existing labeled messages also run through the program, and it appears as of now the page must be refreshed before the labels show up. But still, a good start :-)

--- delete.js
+++ delete.js
@@ -1,53 +1,36 @@
 // This file is used for development purposes only, and will be deleted in final release versions.
 
 
-// compose.js:21 {
-//  "threads": [
-//   {
-//    "id": "14e69be815c6a058",
-//    "snippet": "Thaaaanks",
-//    "historyId": "9120"
-//   },
-//   {
-//    "id": "14e69c9075bd531c",
-//    "snippet": "Hi On Jul 7, 2015 1:30 PM, &quot;Dan Klos&quot; &lt;dan@pledgmail.com&gt; wrote: Thank you!",
-//    "historyId": "8926"
-//   },
-//   {
-//    "id": "14e6a27a0c42a6a7",
-//    "snippet": "This is my wonderful reply On Tue, Jul 7, 2015 at 1:14 PM, Dan Klos &lt;dan@pledgmail.com&gt; wrote:",
-//    "historyId": "8702"
-//   },
-//   {
-//    "id": "14e644211d19b0b8",
-//    "snippet": "Here&#39;s the reply, my good friend :-) On Mon, Jul 6, 2015 at 9:45 AM, Dan Klos &lt;dan@pledgmail.",
-//    "historyId": "9020"
-//   },
-//   {
-//    "id": "14e13259f00f0e73",
-//    "snippet": "Hello Daniel Klos, Thanks for buying from Chrome Web Store using Google Wallet! Chrome Web Store will",
-//    "historyId": "8431"
-//   },
-//   {
-//    "id": "14e12da5ca9c1614",
-//    "snippet": "Here are your account details. Sign in » Your billing setup is complete. See your account details",
-//    "historyId": "6181"
-//   },
-//   {
-//    "id": "14e12d1e3e41baf5",
-//    "snippet": "Hi Dan Welcome to your Gmail inbox Save everything With up to 30GB of space, you&#39;ll never need to",
-//    "historyId": "2678"
-//   },
-//   {
-//    "id": "14e12d1e1be7b3e2",
-//    "snippet": "Hi Dan Get the official Gmail app The best features of Gmail are only available on your phone and",
-//    "historyId": "6114"
-//   },
-//   {
-//    "id": "14e12d1e19e86589",
-//    "snippet": "Hi Dan Work smarter with Gmail and Google Apps Manage Calendar meetings Google Calendar makes",
-//    "historyId": "2682"
-//   }
-//  ],
-//  "resultSizeEstimate": 9
-// }
+var gapiGETRequest = function (gapiRequestURL)
+  {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open( "GET", gapiRequestURL, false );
+      xmlHttp.send( null );
+      return xmlHttp.responseText;
+  }
+
+var gapiRequestInboxMessagesAndToken = "https://www.googleapis.com/gmail/v1/users/me/messages?q=-label%3ASENT+in%3AINBOX&access_token=" + thisToken
+var allMessagesReceived = gapiGETRequest(gapiRequestInboxMessagesAndToken)
+var allMessagesObject = JSON.parse(allMessagesReceived)
+var messageIdsOfReceivedMessages = [];
+var getIdsOfReceivedMessages = function(responseObject){
+  for(var i=0; i < responseObject.messages.length; i ++) {
+    messageIdsOfReceivedMessages.push(responseObject.messages[i].id);
+  }
+}
+
+var messageContentsArr = [];
+var getMessageContents = function(messageIdList)
+{
+  for(var i=0; i < messageIdList.length; i++)
+  {
+    var gapiRequestMessageWithId = "https://www.googleapis.com/gmail/v1/users/me/messages/" + messageIdList[i] + "?access_token=" + thisToken
+    var currentMessage = JSON.parse(gapiGETRequest(gapiRequestMessageWithId))
+    var encodedMessageContents = currentMessage.payload.parts[0].body.data
+    var decodedMessageContents = atob(encodedMessageContents.replace(/-/g, '+').replace(/_/g, '/'));
+    messageContentsArr.push(decodedMessageContents)
+  }
+}
+
+getIdsOfReceivedMessages(allMessagesObject);
+getMessageContents(messageIdsOfReceivedMessages);
--- scripts/background.js
+++ scripts/background.js
@@ -1,269 +1,179 @@
-//////////////// BEGIN TESTING
-  var background = chrome.extension.getBackgroundPage();
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+    chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+      thisToken = token
+      // alert("HERE")
+      chrome.runtime.onMessage.addListener(
+        function(request,sender,sendResponse){
+          var gapiRequestInboxThreadsAndToken = "https://www.googleapis.com/gmail/v1/users/me/threads?q=-from%3Ame+in%3Ainbox&access_token=" + thisToken
+          var gapiRequestInboxMessagesAndToken = "https://www.googleapis.com/gmail/v1/users/me/messages?q=-label%3ASENT+in%3AINBOX&access_token=" + thisToken
 
-// var nonAuthApiKey = "AIzaSyB2V-yBQqZB6U3vM4urVKpBoksYXW2IeMs"
-
-// $.getScript('https://apis.google.com/js/client.js?onload=init', function(){
-
-  // alert("Inside the script")
-  // function init() {
-  //   gapi.client.setApiKey(nonAuthApiKey);
-  //   alert("Set API key fired")
-  // }
-
-
-
-  chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-      chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-        thisToken = token
-        // alert("HERE")
-        chrome.runtime.onMessage.addListener(
-          function(request,sender,sendResponse){
-            var gapiRequestInboxThreadsAndToken = "https://www.googleapis.com/gmail/v1/users/me/threads?q=-from%3Ame+in%3Ainbox&access_token=" + thisToken
-            var gapiRequestInboxMessagesAndToken = "https://www.googleapis.com/gmail/v1/users/me/messages?q=-label%3ASENT+in%3AINBOX&access_token=" + thisToken
-
-
-            var gapiGETRequest = function (gapiRequestURL)
-              {
-                  var xmlHttp = new XMLHttpRequest();
-                  xmlHttp.open( "GET", gapiRequestURL, false );
-                  xmlHttp.send( null );
-                  return xmlHttp.responseText;
-              }
-
-            var allThreads = gapiGETRequest(gapiRequestInboxThreadsAndToken);
-            var allMessagesReceived = gapiGETRequest(gapiRequestInboxMessagesAndToken)
-            var allThreadsObject = JSON.parse(allThreads);
-            var allMessagesObject = JSON.parse(allMessagesReceived)
-            var messageIdsOfMessagesWithContent = [];
-            var getIdsOfMessagesWithContents = function(responseObject){
-              for(var i=0; i < responseObject.messages.length; i ++) {
-                messageIdsOfMessagesWithContent.push(responseObject.messages[i].id);
-              }
-            }
-
-            var messageContentsArr = [];
-            var gapiRequestMessageWithId = "";
-            var getMessageContents = function(messageIdList)
+          var gapiGETRequest = function (gapiRequestURL)
             {
-              for(var i=0; i < messageIdList.length; i++)
-              {
-                gapiRequestMessageWithId = "https://www.googleapis.com/gmail/v1/users/me/messages/" + messageIdList[i] + "?access_token=" + thisToken
-                var currentMessage = JSON.parse(gapiGETRequest(gapiRequestMessageWithId))
-                var encodedMessageContents = currentMessage.payload.parts[0].body.data
-                var decodedMessageContents = atob(encodedMessageContents.replace(/-/g, '+').replace(/_/g, '/'));
-                messageContentsArr.push(decodedMessageContents)
-              }
+                var xmlHttp = new XMLHttpRequest();
+                xmlHttp.open( "GET", gapiRequestURL, false );
+                xmlHttp.send( null );
+                return xmlHttp.responseText;
             }
 
-            getIdsOfMessagesWithContents(allMessagesObject);
-            getMessageContents(messageIdsOfMessagesWithContent);
-
-            var labelsRequestURL = "https://www.googleapis.com/gmail/v1/users/me/labels?access_token=" + thisToken;
-
-            var createLabel = function (gapiRequestURL, labelName)
-            {
-
-              $.ajax({
-                url: gapiRequestURL,
-                method: "POST",
-                contentType: "application/json",
-                data: JSON.stringify({
-                  name: labelName,
-                  labelListVisibility: "labelShow",
-                  messageListVisibility: "show"
-                }),
-                success: function(msg){
-                  alert(JSON.stringify(msg));
-                },
-                error: function(msg){
-                  alert("Error:" + JSON.stringify(msg));
-                }
-              })
+          var allMessagesReceived = gapiGETRequest(gapiRequestInboxMessagesAndToken)
+          var allMessagesObject = JSON.parse(allMessagesReceived)
+          var messageIdsOfMessagesWithContent = [];
+          var getIdsOfMessagesWithContents = function(responseObject){
+            for(var i=0; i < responseObject.messages.length; i ++) {
+              messageIdsOfMessagesWithContent.push(responseObject.messages[i].id);
             }
-
-            var existingLabels = JSON.parse(gapiGETRequest(labelsRequestURL))
-            var labelNameForMessageContentTooLong = "TooLong"
-            var labelNameForConciseMessageContent = "Concise"
-            var makePledgmailLabelsIfNecessary = function(){
-              var labelTooLongExists = false;
-              var labelConciseExists = false;
-              for (var i=0; i < existingLabels.labels.length; i++){
-                if (existingLabels.labels[i].name === labelNameForMessageContentTooLong){
-                  labelTooLongExists = true;
-                }
-                else if (existingLabels.labels[i].name === labelNameForConciseMessageContent){
-                  labelConciseExists = true;
-                }
-              }
-              if (labelTooLongExists === false && labelConciseExists === false){
-                createLabel(labelsRequestURL, labelNameForMessageContentTooLong);
-                createLabel(labelsRequestURL, labelNameForConciseMessageContent);
-              }
-              else if (labelTooLongExists === false){
-                createLabel(labelsRequestURL, labelNameForMessageContentTooLong);
-              }
-              else if (labelConciseExists === false){
-                createLabel(labelsRequestURL, labelNameForConciseMessageContent);
-              }
-            }
-
-            makePledgmailLabelsIfNecessary();
-
-
-
-
-
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-              chrome.tabs.sendMessage(tabs[0].id, {data: existingLabels}, function(response) {
-                // alert(response.farewell);
-              });
-            });
-
-
-              // var getListOfThreadsFromGAPI = function listThreads(userId, callback) {
-              //   // alert("I AM HERE 2222")
-              //   var getPageOfThreads = function(request, result) {
-              //     request.execute(function (resp) {
-              //       // alert("I AM HERE 4444")
-              //       result = result.concat(resp.threads);
-              //       var nextPageToken = resp.nextPageToken;
-              //       if (nextPageToken) {
-              //         request = gapi.client.gmail.users.threads.list({
-              //           'userId': me,
-              //           'pageToken': nextPageToken
-              //         });
-              //         getPageOfThreads(request, result);
-              //       } else {
-              //         callback(result);
-              //       }
-              //     });
-              //   };
-              //   // alert("I AM HERE 33333")
-              //   // alert(thisToken)
-              //   // alert(gapi)
-              //   var request = gapi.client.gmail.users.threads.list({
-              //     'userId': userId
-              //   });
-              //   // alert("I AM HERE 4444")
-              //   getPageOfThreads(request, []);
-              // }
-
-              // // alert("Before function execution")
-              // var testListOfThreads = getListOfThreadsFromGAPI("me")
-
-              // // alert(testListOfThreads)
-
-
-
-
-            // sendResponse({token: thisToken});
           }
-        );
-      });
-    }
-  })
-// });
+
+          var messageContentsArr = [];
+          var gapiRequestMessageWithId = "";
+          var getMessageContents = function(messageIdList)
+          {
+            for(var i=0; i < messageIdList.length; i++)
+            {
+              gapiRequestMessageWithId = "https://www.googleapis.com/gmail/v1/users/me/messages/" + messageIdList[i] + "?access_token=" + thisToken
+              var currentMessage = JSON.parse(gapiGETRequest(gapiRequestMessageWithId))
+              // var encodedMessageContents = currentMessage.payload.parts[0].body.data
+              // var decodedMessageContents = atob(encodedMessageContents.replace(/-/g, '+').replace(/_/g, '/'));
+              // messageContentsArr.push(decodedMessageContents)
+              messageContentsArr.push(currentMessage)
+            }
+          }
+
+          getIdsOfMessagesWithContents(allMessagesObject);
+          getMessageContents(messageIdsOfMessagesWithContent);
+
+          var labelsRequestURL = "https://www.googleapis.com/gmail/v1/users/me/labels?access_token=" + thisToken;
+          var createLabel = function (gapiRequestURL, labelName)
+          {
+
+            $.ajax({
+              url: gapiRequestURL,
+              method: "POST",
+              contentType: "application/json",
+              data: JSON.stringify({
+                name: labelName,
+                labelListVisibility: "labelShow",
+                messageListVisibility: "show"
+              }),
+              success: function(msg){
+                // alert(JSON.stringify(msg));
+              },
+              error: function(msg){
+                alert("Error:" + JSON.stringify(msg));
+              }
+            })
+          }
+
+          var existingLabels = JSON.parse(gapiGETRequest(labelsRequestURL))
+          var labelNameForMessageContentTooLong = "TooLong"
+          var labelNameForConciseMessageContent = "Concise"
+          var makePledgmailLabelsIfNecessary = function(){
+            var labelTooLongExists = false;
+            var labelConciseExists = false;
+            for (var i=0; i < existingLabels.labels.length; i++){
+              if (existingLabels.labels[i].name === labelNameForMessageContentTooLong){
+                labelTooLongExists = true;
+              }
+              else if (existingLabels.labels[i].name === labelNameForConciseMessageContent){
+                labelConciseExists = true;
+              }
+            }
+            if (labelTooLongExists === false && labelConciseExists === false){
+              createLabel(labelsRequestURL, labelNameForMessageContentTooLong);
+              createLabel(labelsRequestURL, labelNameForConciseMessageContent);
+            }
+            else if (labelTooLongExists === false){
+              createLabel(labelsRequestURL, labelNameForMessageContentTooLong);
+            }
+            else if (labelConciseExists === false){
+              createLabel(labelsRequestURL, labelNameForConciseMessageContent);
+            }
+          }
+          makePledgmailLabelsIfNecessary();
+
+
+          var applyTooLongLabel = function (gapiRequestURL, labelIdsArr)
+          {
+
+            $.ajax({
+              url: gapiRequestURL,
+              method: "POST",
+              contentType: "application/json",
+              data: JSON.stringify({
+                addLabelIds: labelIdsArr
+              }),
+              success: function(msg){
+                // alert(JSON.stringify(msg));
+              },
+              error: function(msg){
+                alert("Error:" + JSON.stringify(msg));
+              }
+            })
+          }
+
+          var decideWhichLabelToApply = function(charLimit){
+            var tempCharLimit = 640;
+            var tempLabelIdTooLong = "Label_12"
+            var tempLabelIdConcise = "Label_13"
+            var labelIdsArr = []
+            var messageID = 0;
+            for(var i=0; i < messageContentsArr.length; i++){
+              var currentMessage = messageContentsArr[i]
+              var encodedMessageContents = currentMessage.payload.parts[0].body.data
+              var decodedMessageContents = atob(encodedMessageContents.replace(/-/g, '+').replace(/_/g, '/'));
+
+              if (decodedMessageContents.length > tempCharLimit){
+                messageID = currentMessage.id
+                var labelModifyURL = "https://www.googleapis.com/gmail/v1/users/me/messages/" + messageID + "/modify?access_token=" + thisToken
+                labelIdsArr.push(tempLabelIdTooLong)
+                applyTooLongLabel(labelModifyURL, labelIdsArr)
+              }
+              else if (decodedMessageContents.length <= tempCharLimit) {
+                console.log("Concise")
+              }
+            }
+          }
+
+          decideWhichLabelToApply()
+
+
+          chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {data: "Hi"}, function(response) {
+            });
+          });
+        }
+      );
+    });
+  }
+})
 
 
 
 
 
 
-//////////////// END TESTING
 
 
 
-// console.log("Background.js is loading ******************************")
-
-// //oauth2 auth
-// chrome.identity.getAuthToken(
-//   {'interactive': true},
-//   function(){
-//     //load Google's javascript client libraries
-//     window.gapi_onload = authorize;
-//     loadScript('https://apis.google.com/js/client.js');
-//   }
-// );
 
 
-// function loadScript(url){
-//   var request = new XMLHttpRequest();
-
-//   request.onreadystatechange = function(){
-//     if(request.readyState !== 4) {
-//       return;
-//     }
-
-//     if(request.status !== 200){
-//       return;
-//     }
-
-//     eval(request.responseText);
-//   };
-
-//   request.open('GET', url);
-//   request.send();
-// }
-
-// function authorize(){
-//   console.log("This is the authorize function")
-//   gapi.auth.authorize(
-//     {
-//       client_id: '887620817404-05g424mgdqpa6dm3trah3h83jbrtk9di.apps.googleusercontent.com',
-//       immediate: true,
-//       scope: 'https://www.googleapis.com/auth/gmail.modify'
-//     },
-//     function(){
-//       gapi.client.load('gmail', 'v1', gmailAPILoaded);
-//     }
-//   );
-// }
-
-// function gmailAPILoaded(){
-//     //do stuff here
-// }
 
 
-// /* here are some utility functions for making common gmail requests */
-// function getThreads(query, labels){
-//   return gapi.client.gmail.users.threads.list({
-//     userId: 'me',
-//     q: query, //optional query
-//     labelIds: labels //optional labels
-//   }); //returns a promise
-// }
 
-// //takes in an array of threads from the getThreads response
-// function getThreadDetails(threads){
-//   var batch = new gapi.client.newBatch();
 
-//   for(var ii=0; ii<threads.length; ii++){
-//     batch.add(gapi.client.gmail.users.threads.get({
-//       userId: 'me',
-//       id: threads[ii].id
-//     }));
-//   }
 
-//   return batch;
-// }
 
-// function getThreadHTML(threadDetails){
-//   var body = threadDetails.result.messages[0].payload.parts[1].body.data;
-//   return B64.decode(body);
-// }
 
-// function archiveThread(id){
-//   var request = gapi.client.request(
-//     {
-//       path: '/gmail/v1/users/me/threads/' + id + '/modify',
-//       method: 'POST',
-//       body: {
-//         removeLabelIds: ['INBOX']
-//       }
-//     }
-//   );
 
-//   request.execute();
-// })
+
+
+
+
+
+
+
+
+
+
+
